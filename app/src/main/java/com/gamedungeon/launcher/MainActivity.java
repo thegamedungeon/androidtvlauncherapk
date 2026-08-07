083cb631e09080ffc7e410efbe4f6d8e3ecd0f61
crib <alexdapro.ssp@gmail.com>
2026-08-07 15:05:42 -0500

    Update MainActivity.java

--- app/src/main/java/com/gamedungeon/launcher/MainActivity.java
+++ app/src/main/java/com/gamedungeon/launcher/MainActivity.java
@@ -1,19 +1,30 @@
 package com.gamedungeon.launcher;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Base64;
 import android.webkit.JavascriptInterface;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import androidx.appcompat.app.AppCompatActivity;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
     private WebView webView;
 
     @Override
@@ -21,58 +32,101 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         webView = new WebView(this);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setDomStorageEnabled(true);
-
-        // Expose native methods to JavaScript under window.AndroidBridge
-        webView.addJavascriptInterface(new WebAppInterface(), "AndroidBridge");
-
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                // Load installed apps right after page loads
-                webView.loadUrl("javascript:initApps()");
-            }
-        });
-
-        // Load local index.html from assets folder
-        webView.loadUrl("file:///android_asset/index.html");
         setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.addJavascriptInterface(new WebAppInterface(this), "AndroidBridge");
+
+        webView.loadUrl("file:///android_asset/index.html");
     }
 
-    // Intercept hardware Back Button to return to Home view instead of closing
     @Override
     public void onBackPressed() {
-        webView.loadUrl("javascript:goHome()");
+        // Prevent back button from exiting launcher
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
     }
 
     public class WebAppInterface {
+        Context mContext;
+
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
         @JavascriptInterface
         public String getInstalledApps() {
-            PackageManager pm = getPackageManager();
+            JSONArray appList = new JSONArray();
+            PackageManager pm = mContext.getPackageManager();
+
             Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
             mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-            List<ResolveInfo> pkgList = pm.queryIntentActivities(mainIntent, 0);
 
-            JSONArray appsArray = new JSONArray();
-            for (ResolveInfo ri : pkgList) {
+            List<ResolveInfo> pkgAppsList = pm.queryIntentActivities(mainIntent, 0);
+
+            for (ResolveInfo ri : pkgAppsList) {
                 try {
-                    JSONObject appObj = new JSONObject();
-                    appObj.put("name", ri.loadLabel(pm).toString());
-                    appObj.put("packageName", ri.activityInfo.packageName);
-                    appsArray.put(appObj);
+                    JSONObject appInfo = new JSONObject();
+                    String label = ri.loadLabel(pm).toString();
+                    String packageName = ri.activityInfo.packageName;
+
+                    // Exclude self from list
+                    if (packageName.equals(mContext.getPackageName())) continue;
+
+                    Drawable icon = ri.loadIcon(pm);
+                    String base64Icon = drawableToBase64(icon);
+
+                    appInfo.put("name", label);
+                    appInfo.put("package", packageName);
+                    appInfo.put("icon", base64Icon);
+
+                    appList.put(appInfo);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            return appsArray.toString();
+            return appList.toString();
         }
 
         @JavascriptInterface
         public void launchApp(String packageName) {
-            Intent launchIntent = getPackageManager().getLaunchIntentForPackage(packageName);
+            Intent launchIntent = mContext.getPackageManager().getLaunchIntentForPackage(packageName);
             if (launchIntent != null) {
-                startActivity(launchIntent);
+                mContext.startActivity(launchIntent);
+            }
+        }
+
+        private String drawableToBase64(Drawable drawable) {
+            try {
+                Bitmap bitmap;
+                if (drawable instanceof BitmapDrawable) {
+                    bitmap = ((BitmapDrawable) drawable).getBitmap();
+                } else {
+                    bitmap = Bitmap.createBitmap(
+                        Math.max(1, drawable.getIntrinsicWidth()),
+                        Math.max(1, drawable.getIntrinsicHeight()),
+                        Bitmap.Config.ARGB_8888
+                    );
+                    Canvas canvas = new Canvas(bitmap);
+                    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+                    drawable.draw(canvas);
+                }
+
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                byte[] byteArray = outputStream.toByteArray();
+                return "data:image/png;base64," + Base64.encodeToString(byteArray, Base64.NO_WRAP);
+            } catch (Exception e) {
+                return "";
             }
         }
     }
